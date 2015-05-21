485a0e5854be6402580032619e6765d1ecbee717
Hannah Waxman <hannah.waxman8@gmail.com>
2015-05-21 11:06:20 +0100

    Refactored

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,25 +1,23 @@
-/**
- * Created by hannahwaxman on 20/05/15.
- */
-
 import java.util.ArrayList;
 
 public class PrimeNumbers {
 
-        public static void generate(int n) {
-            ArrayList<Integer> primeFactors = new ArrayList<Integer>();
+    public static void main(String[] args) {
+        generate(30);
+    }
 
-            for (int i = 2; i <= n; i ++) {
-                while (n % i == 0) {
-                    primeFactors.add(i);
-                    n /= i;
-                }
+    public static void generate(int n) {
+        ArrayList<Integer> primeFactors = new ArrayList<>();
+
+        for (int i = 2; i <= n; i ++) {
+            if (n % i == 0) {
+                primeFactors.add(i);
+                n /= i;
             }
-            System.out.println(primeFactors);
         }
+        System.out.println(primeFactors);
+    }
 
-        public static void main(String[] args) {
-            generate(30);
-        }
+
 
 }
